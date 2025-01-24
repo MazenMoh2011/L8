ea0ee9465dd510fca64b53b13ef4ae693f346330
MazenMoh2011 <mazenalnagar203@gmail.com>
2025-01-24 15:45:03 +0300

    jinjinnji

---
+++ P.py
@@ -0,0 +1,10 @@
+def shutdown():
+ step1=input("do you have all your background applications closed?")
+ if step1=="yes":
+    step2=input("is your computer broken?")
+    if step2=="no":
+        step3=input("did you download a virus on your computer?")
+        if step3=="no":
+            print("shutting down...")
+ else:
+  print("sorry.")
